d63ec841b7f35c965ba607d1b11c2b9e518bf594
Eder Baldrighi <eder@kanamobi.com.br>
2018-12-06 12:39:00 -0200

    first commit

---
+++ MyFirstLib/MyFirstLib/MyFirstLib.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstLib.h
+//  MyFirstLib
+//
+//  Created by Eder Baldrighi on 06/12/2018.
+//  Copyright © 2018 Eder Baldrighi Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstLib.
+FOUNDATION_EXPORT double MyFirstLibVersionNumber;
+
+//! Project version string for MyFirstLib.
+FOUNDATION_EXPORT const unsigned char MyFirstLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstLib/PublicHeader.h>
+
+
---
+++ MyFirstLib/MyFirstLib/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  MyFirstLib
+//
+//  Created by Eder Baldrighi on 06/12/2018.
+//  Copyright © 2018 Eder Baldrighi Enterprises. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    
+    public static func test() -> String {
+        return "Success test!"
+    }
+}
